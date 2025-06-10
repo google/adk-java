0b61097b2fd450490340b845618b09bf4d09d0b9
Xinhua Gu <xinhua.gu@gmail.com>
2025-06-11 01:36:15 +0200

    feat: Use templating-maven-plugin to sync project version in Version.java

--- core/src/main/java/com/google/adk/Version.java
+++ core/src/main/java/com/google/adk/Version.java
@@ -18,7 +18,7 @@ package com.google.adk;
 
 /** Holding class for the version of the Java ADK. */
 public final class Version {
-  public static final String JAVA_ADK_VERSION = "0.1.0";
+  public static final String JAVA_ADK_VERSION = "${project.version}";
 
   private Version() {}
 }
