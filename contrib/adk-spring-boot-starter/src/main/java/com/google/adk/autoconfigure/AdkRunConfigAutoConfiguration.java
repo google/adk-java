d8d33c083307360d48e0600560ab1649e50fb076
mekkiamiri <mekki.amiri@gmail.com>
2026-06-15 00:48:54 +0200

    feat(spring-boot-starter): expose full operational RunConfig surface
    Addresses ddobrin maintainer note #4.
    
    AdkRunConfigProperties now binds the operational subset called out by the
    reviewer:
    - streamingMode (was already there)
    - maxLlmCalls (default 500, matching RunConfig.builder())
    - toolExecutionMode (default NONE, matching RunConfig.builder())
    - saveInputBlobsAsArtifacts (default false)
    - autoCreateSession (default false, matching the new RunConfig field
      added since the PR was opened)
    
    AdkRunConfigAutoConfiguration applies all five to RunConfig.builder().
    
    Out of scope for this property surface (left to user-defined RunConfig
    beans via @ConditionalOnMissingBean): speechConfig, responseModalities,
    inputAudioTranscription, outputAudioTranscription, avatarConfig.

--- contrib/adk-spring-boot-starter/src/main/java/com/google/adk/autoconfigure/AdkRunConfigAutoConfiguration.java
+++ contrib/adk-spring-boot-starter/src/main/java/com/google/adk/autoconfigure/AdkRunConfigAutoConfiguration.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package com.google.adk.autoconfigure;
 
 import com.google.adk.agents.RunConfig;
@@ -7,15 +16,20 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+/** Auto-configures the {@link RunConfig} bean from {@code adk.run-config.*} properties. */
 @AutoConfiguration
 @EnableConfigurationProperties(AdkRunConfigProperties.class)
 public class AdkRunConfigAutoConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean
-    public RunConfig runConfig(AdkRunConfigProperties properties) {
-        return RunConfig.builder()
-                .setStreamingMode(properties.getStreamingMode())
-                .build();
-    }
+  @Bean
+  @ConditionalOnMissingBean
+  public RunConfig runConfig(AdkRunConfigProperties properties) {
+    return RunConfig.builder()
+        .setStreamingMode(properties.getStreamingMode())
+        .setMaxLlmCalls(properties.getMaxLlmCalls())
+        .setToolExecutionMode(properties.getToolExecutionMode())
+        .setSaveInputBlobsAsArtifacts(properties.isSaveInputBlobsAsArtifacts())
+        .setAutoCreateSession(properties.isAutoCreateSession())
+        .build();
+  }
 }
